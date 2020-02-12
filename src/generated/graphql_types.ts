22e9f3291c08c459e992fd2efe8ac4d88a5356ba
takaaa220 <taaaaaaaaaa220@gmail.com>
2020-02-12 22:53:52 +0900

    code gen

---
+++ src/generated/graphql_types.ts
@@ -0,0 +1,170 @@
+import { GraphQLResolveInfo } from 'graphql';
+export type Maybe<T> = T | null;
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
+};
+
+export type Mutation = {
+   __typename?: 'Mutation',
+  addUser: User,
+  deleteUser: User,
+};
+
+
+export type MutationAddUserArgs = {
+  lastName: Scalars['String'],
+  firstName: Scalars['String'],
+  age: Scalars['Int']
+};
+
+
+export type MutationDeleteUserArgs = {
+  id: Scalars['Int']
+};
+
+export type Query = {
+   __typename?: 'Query',
+  user?: Maybe<User>,
+  users?: Maybe<Array<User>>,
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['Int']
+};
+
+export type User = {
+   __typename?: 'User',
+  id: Scalars['Int'],
+  firstName: Scalars['String'],
+  lastName: Scalars['String'],
+  age: Scalars['Int'],
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+
+export type StitchingResolver<TResult, TParent, TContext, TArgs> = {
+  fragment: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | StitchingResolver<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes>;
+
+export type isTypeOfResolverFn<T = {}> = (obj: T, info: GraphQLResolveInfo) => boolean;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Query: ResolverTypeWrapper<{}>,
+  Int: ResolverTypeWrapper<Scalars['Int']>,
+  User: ResolverTypeWrapper<User>,
+  String: ResolverTypeWrapper<Scalars['String']>,
+  Mutation: ResolverTypeWrapper<{}>,
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>,
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Query: {},
+  Int: Scalars['Int'],
+  User: User,
+  String: Scalars['String'],
+  Mutation: {},
+  Boolean: Scalars['Boolean'],
+};
+
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  addUser?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<MutationAddUserArgs, 'lastName' | 'firstName' | 'age'>>,
+  deleteUser?: Resolver<ResolversTypes['User'], ParentType, ContextType, RequireFields<MutationDeleteUserArgs, 'id'>>,
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>,
+  users?: Resolver<Maybe<Array<ResolversTypes['User']>>, ParentType, ContextType>,
+};
+
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
+  firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>,
+  age?: Resolver<ResolversTypes['Int'], ParentType, ContextType>,
+  __isTypeOf?: isTypeOfResolverFn<ParentType>,
+};
+
+export type Resolvers<ContextType = any> = {
+  Mutation?: MutationResolvers<ContextType>,
+  Query?: QueryResolvers<ContextType>,
+  User?: UserResolvers<ContextType>,
+};
+
+
+/**
+ * @deprecated
+ * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
+*/
+export type IResolvers<ContextType = any> = Resolvers<ContextType>;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,61 @@
 import "reflect-metadata";
-import { ApolloServer, gql } from "apollo-server";
-import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server";
+import { createConnection, Connection } from "typeorm";
+import { importSchema } from "graphql-import";
+import {
+  QueryResolvers,
+  MutationResolvers,
+  Resolvers,
+} from "./generated/graphql_types";
+import { User } from "./entity/User";
+
+type Context = {
+  connection: Connection;
+};
 
 const main = async () => {
   try {
     const connection = await createConnection();
 
-    // The GraphQL schema
-    const typeDefs = gql`
-      type Query {
-        "A simple type for getting started!"
-        hello: String
-      }
-    `;
+    const Query: QueryResolvers<Context> = {
+      user: async (_parent, { id }, { connection }, _info) => {
+        const user = await connection.getRepository(User).findOne({ id });
 
-    // A map of functions which return data for the schema.
-    const resolvers = {
-      Query: {
-        hello: () => "world",
+        return user || null;
       },
+      users: async (_parent, _args, { connection }, _info) => {
+        const users = await connection.getRepository(User).find();
+
+        return users || [];
+      },
+    };
+
+    const Mutation: MutationResolvers<Context> = {
+      addUser: async (_parent, args, { connection }, _info) => {
+        const repository = connection.getRepository(User);
+
+        const newUser = new User();
+        newUser.lastName = args.lastName;
+        newUser.firstName = args.firstName;
+        newUser.age = args.age;
+
+        const user = await repository.save(newUser);
+        return user || null;
+      },
+      deleteUser: async (_parent, { id }, { connection }, _info) => {
+        const repository = await connection.getRepository(User);
+
+        const user = repository.findOne({ id });
+        await repository.delete({ id });
+
+        return user;
+      },
+    };
+
+    const typeDefs = await importSchema("graphql/schema.graphql");
+    const resolvers: Resolvers = {
+      Query,
+      Mutation,
     };
 
     const server = new ApolloServer({
